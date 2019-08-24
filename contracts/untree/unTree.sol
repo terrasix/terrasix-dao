2e7d0ef87efe7151897be30da41b1b9002553b30
Benjamin Bollen <ben@ost.com>
2019-08-24 23:33:24 +0200

    issueUntrees

---
+++ contracts/untree/unTree.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+contract Untree is ERC20 {
+
+
+}
